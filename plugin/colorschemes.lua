8f16a49eec21f525120427a92e8aab648f3eb176
mvayк <mvayk@proton.me>
2025-05-31 16:45:51 +1000

    transparency

--- plugin/colorschemes.lua
+++ plugin/colorschemes.lua
@@ -1,3 +1,11 @@
 -- vim.cmd[[highlight Pmenu ctermbg=black guibg=black]]
 --// vim.g.material_style = "deep ocean"
 vim.cmd[[colorscheme material-deep-ocean]]
+
+vim.cmd [[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+  hi EndOfBuffer guibg=NONE ctermbg=NONE
+  hi SignColumn guibg=NONE ctermbg=NONE
+  hi VertSplit guibg=NONE ctermbg=NONE
+]]
