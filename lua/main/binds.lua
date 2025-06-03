9ff4be0b9cfd36af95b3fe82f1754adeaf782d7f
mvayк <mvayk@proton.me>
2025-06-03 20:07:22 +1000

    noh

--- lua/main/binds.lua
+++ lua/main/binds.lua
@@ -2,3 +2,9 @@ vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-n>", ":bnext<CR>")
 vim.keymap.set("n", "<C-p>", ":bprev<CR>")
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.api.nvim_create_autocmd("CursorMoved", {
+  callback = function()
+    vim.cmd("nohlsearch")
+  end,
+})
